aa7698dc18ce1b7e15685cc3869012179d601283
Milan <milanrathod5201@gmail.com>
2026-08-22 12:19:30 +0530

    test(integration): implement end-to-end multi-layer validation workflows (#961)

---
+++ tests/conftest.py
@@ -0,0 +1,7 @@
+import sys
+import os
+
+# Add the project root directory to sys.path so that 'tests' package can be imported
+sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
+
+pytest_plugins = ["tests.fixtures.conftest"]
